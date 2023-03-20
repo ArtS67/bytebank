9abf9a28381530873a92ea97b07bff1ae354f18e
Arthur Elias Da Silva Simões Leão <71152532+ArtS67@users.noreply.github.com>
2023-03-20 10:24:34 -0300

    atualização da classe conta

--- Conta.java
+++ Conta.java
@@ -1,30 +1,83 @@
-package bytebank;
+package br.com.bytebank.banco.modelo;
 
-public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
-	String titular;
+public abstract class Conta {
+	protected double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+
+	public Conta(int Agencia, int Numero) {
+		if (Agencia < 1) {
+			throw new IllegalArgumentException("Agencia da conta inválido");
+		}
+		if (Numero < 1) {
+			throw new IllegalArgumentException("Numero da conta inválido");
+		}
+		Conta.total++;
+		System.out.println("Conta Nova Criada\n" + "Conta computada de N° " + Conta.total);
+		this.agencia = Agencia;
+		this.numero = Numero;
+		System.out.println("Agencia: " + Agencia + "\n" + "Numero da Conta: " + Numero +  "\n");
+		System.out.println("------------------------------------------------------\n");
+
+	}
 
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
 
-	public boolean saca(double valor) {
-		if (this.saldo >= valor) {
-			this.saldo -= valor;
-			return true;
+	public void saca(double valor) throws SacaExcepton {
+		if (this.saldo < valor) {
+			throw new SacaExcepton("Saldo: " + this.saldo + ", valor: " + valor);
 		}
-		return false;
+		this.saldo -= valor;
+	}
+
+	public void transfere(double valor, Conta destino) throws SacaExcepton {
+		this.saca(valor);
+		destino.deposita(valor);
 
 	}
 
-	public boolean transfere(double valor, Conta destino) {
-		if (this.saldo >= valor) {
-			this.saldo -= valor;
-			destino.deposita(valor);
-			return true;
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("nao pode valor menor igual a 0");
+			return;
 		}
-		return false;
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("nao pode valor menor igual a 0");
+			return;
+		}
+
+		this.agencia = agencia;
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public static int getTotal() {
+		return total;
 	}
 }
